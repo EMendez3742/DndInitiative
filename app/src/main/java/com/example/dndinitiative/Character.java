2617129b677177bdc4587f304b2944eade8e8a80
Eric E. Mendez <ericemendez61@aol.com>
2021-12-04 18:08:39 -0500

    Initial commit

---
+++ app/src/main/java/com/example/dndinitiative/Character.java
@@ -0,0 +1,70 @@
+package com.example.dndinitiative;
+
+/* This is the Character class, which includes information on Hp, spell slots,
+initiative and other characer related information.
+*/
+public class Character {
+    int spellSlot;
+    int hp;
+    float initiative;
+    String name;
+    String imageUrl;
+
+    // getters and setters
+
+    int getSpellSlot(){
+        return spellSlot;
+    }
+
+    void setSpellSlot(int slots){
+        spellSlot = slots;
+    }
+
+    int getHp(){
+        return hp;
+    }
+
+    void setHp(int points){
+        hp = points;
+    }
+
+    float getInitiative(){
+        return initiative;
+    }
+
+    void setInitiative(int init){
+        initiative = init;
+    }
+
+    String getName(){
+        return name;
+    }
+
+    void setName(String n){
+        name = n;
+    }
+
+    String getImageUrl(){
+        return imageUrl;
+    }
+
+    void setImageUrl(String url){
+        imageUrl = url;
+    }
+
+    // hp methods
+    void takeHp(int points){
+        hp -= points;
+    }
+    void addHp(int points){
+        hp += points;
+    }
+
+    // spell slot methods
+    void takeSpellSlot(int slots){
+        spellSlot -= slots;
+    }
+    void addSpellSlot(int slots){
+        spellSlot += slots;
+    }
+}
---
+++ app/src/main/java/com/example/dndinitiative/README.md
@@ -0,0 +1,40 @@
+# DndInitiative
+* By Eric E. Mendez
+* An App that plays through D&amp;D initiative order of multiple characters.
+
+## Table of Contents
+* [Technology used](#tech)
+* [Description](#desc)
+* [Setup/Installation](#setup)
+* [Bugs](#bugs)
+* [Contributing](#contrib)
+* [Licensing](#license)
+
+## Technology used <a name="tech">
+  To be specified
+## Description <a name="desc">
+An app designed to play through D&amp;D initiative order of multiple characters
+### Goals
+#### Functional Requirements:
+* Sort intitiave order
+* Heal and take hp
+* Use and Regain Spell Slots
+* Show hp
+* Cycle through players
+#### Nonfunctional Requirements:
+* Written in Java or swift
+* Android/Apple?
+* Use MySQL for database access
+#### Potential Extras:
+
+## Setup/Installation <a name="setup">
+To be filled out
+
+## Bugs <a name="bugs">
+N/A
+
+## Contributing <a name="contrib">
+Eric Mendez
+
+## Licensing <a name="license">
+N/A
